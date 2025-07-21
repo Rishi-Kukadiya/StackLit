27c2325294ce591d4984a933a1d5857977473412
kenil-rajodiya <kenilarajodiya2005@gmail.com>
2025-07-21 09:41:39 +0530

    cors error solved

--- Backend/app.js
+++ Backend/app.js
@@ -6,8 +6,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['set-cookie']
 }))
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
