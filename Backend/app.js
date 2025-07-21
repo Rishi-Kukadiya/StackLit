91492e24efc51924d70d6ed9b12021121de43aa1
kenil-rajodiya <kenilarajodiya2005@gmail.com>
2025-07-21 09:57:48 +0530

    error solved

--- Backend/app.js
+++ Backend/app.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    origin: process.env.CORS_ORIGIN ,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
