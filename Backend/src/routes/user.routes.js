6abc9bc25c08a10dcb5df2b9ec85fe7c440e0d36
kenil-rajodiya <kenilarajodiya2005@gmail.com>
2025-07-24 16:24:49 +0530

    getUsers controller done

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
-import { changeCurrentPassword, loginUser, logoutUser, refreshAccessToken, registerUser, sendOtp, verifyOtp, forgetPassword } from "../controllers/user.controller.js";
+import { changeCurrentPassword, loginUser, logoutUser, refreshAccessToken, registerUser, sendOtp, verifyOtp, forgetPassword, getUsers } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 import { upload } from "../middlewares/multer.middleware.js";
 import checkOtpVerified from "../middlewares/checkOtpVerified.js";
 const userRouter = Router();
 userRouter.route('/register').post(
     upload.fields([
-        { 
+        {
             name: "avatar",
-            maxCount:1
+            maxCount: 1
         }
     ]),
     registerUser
@@ -20,10 +20,11 @@ userRouter.route("/logout").post(
 )
 userRouter.route("/refresh-token").post(refreshAccessToken)
 userRouter.route("/change-password").post(verifyJWT, changeCurrentPassword)
-userRouter.post("/send-otp" , sendOtp);
-userRouter.post("/verify-otp",  verifyOtp);
+userRouter.post("/send-otp", sendOtp);
+userRouter.post("/verify-otp", verifyOtp);
 
 userRouter.post("/forget-password", checkOtpVerified, forgetPassword);
+userRouter.get("/get-users", getUsers);
 
 
 export default userRouter;
