c95971098349b60c91a07053134a7bd55f4369c9
Rishi <kukadiyarishi895@gmail.com>
2025-07-29 17:57:33 +0530

    Chatbot integrated succesfully

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -17,6 +17,7 @@ import UnansweredQuestion from "./Components/UnanswerQuestion";
 import Users from "./Components/Users";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import Chatbot from "./Components/Chatbot";
 import UserProfile from './components/UserProfile';
 
 function App() {
@@ -39,6 +40,7 @@ function App() {
             <Route path="/users" element={<Users />} />
             <Route path="/unanswered" element={<UnansweredQuestion></UnansweredQuestion>}></Route>
             <Route path="/profile/:userId" element={<UserProfile />} />
+            <Route path="ai-assistance" element={<Chatbot></Chatbot>}></Route>
           </Routes>
         </QuestionProvider>
       </UserProvider>
--- Frontend/src/components/AnswerReaction.jsx
+++ Frontend/src/components/AnswerReaction.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { ThumbsUp, ThumbsDown } from "lucide-react";
 import axios from "axios";
 import ErrorPopup from "./ErrorPopup";
