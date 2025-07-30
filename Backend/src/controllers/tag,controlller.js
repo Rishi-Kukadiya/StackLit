d2e64ad332e01b08443119cc2313686763cef09b
kenil-rajodiya <kenilarajodiya2005@gmail.com>
2025-07-30 16:31:50 +0530

    Tag controllers done

--- Backend/src/controllers/tag,controlller.js
+++ Backend/src/controllers/tag,controlller.js
@@ -1,7 +1,10 @@
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { Question } from "../models/question.model.js";
 import { Tag } from "../models/tag.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import mongoose from "mongoose";
+
 
 const getTags = asyncHandler(async (req, res) => {
     try {
@@ -49,5 +52,53 @@ const getTags = asyncHandler(async (req, res) => {
     }
 });
 
+const getQuestionsByTagId = asyncHandler(async (req, res) => {
+    const { tagId } = req.params;
+    const { page = 1, limit = 10 } = req.query;
 
-export { getTags }
+    if (!mongoose.Types.ObjectId.isValid(tagId)) {
+        return res.json(new ApiError(400, "Invalid tag ID"));
+    }
+    
+    const tag = await Tag.findById(tagId);
+    if (!tag) {
+        return res.json(new ApiError(404, "Tag not found"));
+    }
+
+    const totalQuestions = tag.questions.length;
+
+    const questions = await Question.find({ _id: { $in: tag.questions } })
+        .sort({ createdAt: -1 }) 
+        .skip((page - 1) * limit)
+        .limit(parseInt(limit))
+        .populate("owner", "fullName avatar") 
+        .lean();
+
+    const formattedQuestions = questions.map(q => ({
+        _id: q._id,
+        title: q.title,
+        views: q.views,
+        answers: q.answeredBy.length,
+        owner: {
+            name: q.owner.fullName,
+            avatar: q.owner.avatar
+        },
+        createdAt: q.createdAt
+    }));
+
+
+    return res.json(
+        new ApiResponse(200, {
+            success: true,
+            tagName: tag.name,
+            tagDescription: tag.description,
+            totalQuestions,
+            questions: formattedQuestions,
+            currentPage: parseInt(page),
+            totalPages: Math.ceil(totalQuestions / limit)
+        },"Questins fetched for the Tag")
+    )
+});
+
+
+export { getTags, getQuestionsByTagId };
--- Backend/src/routes/tags.route.js
+++ Backend/src/routes/tags.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { getTags } from "../controllers/tag,controlller.js";
+import { getQuestionsByTagId, getTags } from "../controllers/tag,controlller.js";
 const tagRouter = Router();
 tagRouter.route('/getTags').get(
     getTags
 );
-
+tagRouter.route('/get-questions/:tagId').get(getQuestionsByTagId);
 
 export default tagRouter;
