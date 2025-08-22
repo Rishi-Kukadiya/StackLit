51452ca23c92471a826f0b3008b90ab3627619a5
kenil-rajodiya <kenilarajodiya2005@gmail.com>
2025-08-22 18:59:21 +0530

    deploy fix

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -105,8 +105,10 @@ const loginUser = async (req, res) => {
 
     const options = {
         httpOnly: true,
-        secure: true
-    }
+        secure: true,
+        sameSite: "None",
+    };
+
     // console.log(accessToken);
     // console.log(refreshToken);
 
